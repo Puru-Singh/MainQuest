f781f4170324742728cee15ac7d7bd8e20ad4c19
Puru-Singh <81344325+Puru-Singh@users.noreply.github.com>
2025-08-26 09:53:09 +0530

    repositioning elements

--- script.js
+++ script.js
@@ -73,9 +73,9 @@ document.addEventListener('DOMContentLoaded', () => {
         const gameScreen = `
             <h1>Bug hunting!</h1>
             <p class="hint">Hint: It's something you love, and Karnataka's popular for it.</p>
-            <p class="hint">Pink means that the character is present but position is incorrect.</p>
             <p class="hint">Green means it's present and the position is correct as well.</p>
             <p class="hint">Grey means the position and the character both are incorrect.</p>
+            <p class="hint">Pink means that the character is present but position is incorrect.</p>
             <div id="game-grid"></div>
             <div id="keyboard"></div>
             <div id="feedback" class="feedback"></div>
